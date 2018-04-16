64b6d737713b38b9d9bdc9a236c6376729606a9f
shirleyz9402 <shirleyz9402@yahoo.com>
2018-04-16 17:32:32 -0400

    backend x3

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,26 @@
 document.addEventListener("DOMContentLoaded",() =>{
 const gameContent = document.getElementById('game-content')
+const createPlayer = document.getElementById('create-player')
   function renderForm(){
-    return `<form id="create-player">
-    <label for="new-player-name">Name: </label>
-    <input required type="text" id="new-player-name" name="new-player-name" placeholder="name"><br>
-    <button type="submit" id="create-note">Create Player</button>
-    </form>
+    return `
+    <label for="name">Name: </label>
+    <input required type="text" id="name" name="new-player-name" placeholder="name"><br>
+    <button type="button" id="create-player">Create Player</button>
     `
   }
   gameContent.innerHTML = renderForm()
+
+  gameContent.addEventListener('click', e => {
+    if (e.target.id === 'create-player'){
+    let name = document.getElementById('name').value
+    fetch('http://localhost:3000/api/v1/players', {
+      method: "POST",
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({name: name})
+    })
+    }
+  })
+
 
 
 
