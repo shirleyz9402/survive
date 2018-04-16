d2cd4c0ec268a6c731a8649f0d0f673328e157d0
shirleyz9402 <shirleyz9402@yahoo.com>
2018-04-16 16:29:51 -0400

    backend again

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded",() =>{
+const gameContent = document.getElementById('game-content')
+  function renderForm(){
+    return `<form id="create-player">
+    <label for="new-player-name">Name: </label>
+    <input required type="text" id="new-player-name" name="new-player-name" placeholder="name"><br>
+    <button type="submit" id="create-note">Create Player</button>
+    </form>
+    `
+  }
+  gameContent.innerHTML = renderForm()
 
 
 
